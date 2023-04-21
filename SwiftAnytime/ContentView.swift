257682437498762ddaf272312c6491ecabddabf9
Ritesh Gupta <rg.riteshh@gmail.com>
2023-04-21 18:56:40 +0530

    Step 1

--- SwiftAnytime/ContentView.swift
+++ SwiftAnytime/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+struct AppError {}
+extension AppError: LocalizedError {
+	var errorDescription: String? { "No internet!" }
+}
+
+struct ViewState<T> {
+	let currentState: State
+	let error: Error?
+	let data: T?
+	
+	enum State {
+		case initial
+		case loading
+		case error
+		case data
+	}
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+	let viewState = ViewState<String>.init(currentState: .initial, error: AppError(), data: "nil")
+    
+	var body: some View {
+		ZStack {
+			switch viewState.currentState {
+			case .initial: Text("Initial")
+			case .loading: Text("Loading...")
+			case .error: Text(viewState.error?.localizedDescription ?? "")
+			case .data: Text(viewState.data ?? "")
+			}
+		}
     }
 }
 
