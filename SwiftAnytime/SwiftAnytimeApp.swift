6d590409e14f0dbe38909ca3d36c943bcb89fd22
Ritesh Gupta <rg.riteshh@gmail.com>
2023-04-21 18:13:48 +0530

    Initial Commit

---
+++ SwiftAnytime/SwiftAnytimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAnytimeApp.swift
+//  SwiftAnytime
+//
+//  Created by Ritesh Gupta on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAnytimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
